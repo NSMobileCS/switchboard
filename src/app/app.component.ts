0c6f62543132d20ffe3a114aa6c467591ad400a6
Nathan R Smith <nrsmith012@gmail.com>
2017-10-13 18:45:17 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Switchboard';
+  makeBoxes = (n) => {
+    let boxes = []
+    for (let x=0;x<n;x++){
+      if (Math.random() > 0.5){
+        boxes.push(true);
+      }
+      else {
+        boxes.push(false);
+      }
+    }
+    return boxes;
+  }
+  boxes = this.makeBoxes(8);
+  switchBox = (idx) => {
+    if (this.boxes[idx]){
+      this.boxes[idx] = false;
+    }
+    else {
+      this.boxes[idx] = true;
+    }
+  }
 }
